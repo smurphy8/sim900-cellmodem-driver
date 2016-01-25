1d2418eb776e49dc80ee6ef90cc58efe7aaa7dbd
Scott Murphy <scottmurphy09@gmail.com>
2016-01-24 20:52:49 -0600

    Checking at settings

--- src/System/Sim900CellModem.hs
+++ src/System/Sim900CellModem.hs
@@ -1,3 +1,17 @@
 module System.Sim900CellModem where
 
-import System.Sim900CellModem.Internal
+import           System.Sim900CellModem.Internal
+
+import           System.Hardware.Serialport
+
+
+defaultCellModemPort = "/dev/ttyAMA0"
+
+defaultCellModemSettings = defaultSerialSettings {commSpeed=CS115200}
+
+waitForInput :: IO ()
+waitForInput = do
+   port <- openSerial defaultCellModemPort defaultCellModemSettings
+   rslt <- recv port 9
+   print rslt
+   return ()
